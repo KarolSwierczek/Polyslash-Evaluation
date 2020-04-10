fea6657d2ff604304fb8b3551f029df721152b9b
Karol Świerczek <karolswierczek5@gmail.com>
2020-04-10 14:48:24 +0200

    fix(DoorClosingInterrupt): fixed missing references on project reload

--- Assets/_Scripts/Gameplay/Door/DoorClosingInterrupt.cs
+++ Assets/_Scripts/Gameplay/Door/DoorClosingInterrupt.cs
@@ -1,7 +1,7 @@
 ﻿namespace KarolSwierczek.Elevator.Gameplay
 {
-    using Sirenix.OdinInspector;
     using UnityEngine;
+    using Zenject;
 
     /// <summary>
     /// this class interrupts closing door animation and plays open animation seamlessly
@@ -27,26 +27,19 @@
         {
             _CurrentStateInfo = stateInfo;
         }
+
+        public void OnPlayerEnterCollider()
+        {
+            if (!_StateActive) { _DoorBlocked = true; return; }
+
+            _CurrentAnimator.Play(_DoorOpeningState, 0, 1f - _CurrentStateInfo.normalizedTime);
+        }
+
+        public void OnPlayerExitCollider()
+        {
+            _DoorBlocked = false;
+        }
         #endregion Public Methods
-
-        #region Unity Methods
-        private void OnEnable()
-        {
-            _DoorCollider.OnPlayerEnter += OnPlayerEnterCollider;
-            _DoorCollider.OnPlayerExit += OnPlayerExitCollider;
-        }
-
-        private void OnDisable()
-        {
-            _DoorCollider.OnPlayerEnter -= OnPlayerEnterCollider;
-            _DoorCollider.OnPlayerExit -= OnPlayerExitCollider;
-        }
-        #endregion Unity Methods
-
-        #region Inspector Variables
-        [FoldoutGroup("Refereces")]
-        [SerializeField] private DoorColliderController _DoorCollider;
-        #endregion Inspector Variables
 
         #region Private Variables
         private Animator _CurrentAnimator;
@@ -57,19 +50,5 @@
         private readonly int _DoorOpeningState = Animator.StringToHash("DoorOpening");
         private readonly int _DoorOpenedState = Animator.StringToHash("DoorOpened");
         #endregion Private Variables
-
-        #region Private Methods
-        private void OnPlayerEnterCollider(object sender, DoorColliderController.OnPlayerEnterArgs args)
-        {
-            if (!_StateActive) { _DoorBlocked = true;  return; }
-
-            _CurrentAnimator.Play(_DoorOpeningState, 0, 1f - _CurrentStateInfo.normalizedTime);
-        }
-
-        private void OnPlayerExitCollider(object sender, DoorColliderController.OnPlayerExitArgs args)
-        {
-            _DoorBlocked = false;
-        }
-        #endregion Private Methods
     }
 }
--- Assets/_Scripts/Gameplay/Door/DoorColliderController.cs
+++ Assets/_Scripts/Gameplay/Door/DoorColliderController.cs
@@ -1,7 +1,7 @@
 ﻿namespace KarolSwierczek.Elevator.Gameplay
 {
     using UnityEngine;
-    using System;
+    using Sirenix.OdinInspector;
 
     /// <summary>
     /// this class invokes an event when a player steps into the door collider
@@ -9,27 +9,17 @@
     [RequireComponent(typeof(Collider))]
     public sealed class DoorColliderController : MonoBehaviour
     {
-        #region Public Types
-        public sealed class OnPlayerEnterArgs : EventArgs
-        {
-        }
-
-        public sealed class OnPlayerExitArgs : EventArgs
-        {
-        }
-        #endregion Public Types
-
-        #region Public Variables
-        public event EventHandler<OnPlayerEnterArgs> OnPlayerEnter; 
-        public event EventHandler<OnPlayerExitArgs> OnPlayerExit;
-        #endregion Public Variables
-
         #region Unity Methods
+        private void Awake()
+        {
+            _InterruptStateBehavior = _DoorAnimator.GetBehaviour<DoorClosingInterrupt>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if(other.CompareTag(_PlayerTag))
             {
-                OnPlayerEnter?.Invoke(this, new OnPlayerEnterArgs());
+                _InterruptStateBehavior.OnPlayerEnterCollider();
             }
         }
 
@@ -37,13 +27,19 @@
         {
             if (other.CompareTag(_PlayerTag))
             {
-                OnPlayerExit?.Invoke(this, new OnPlayerExitArgs());
+                _InterruptStateBehavior.OnPlayerExitCollider();
             }
         }
         #endregion Unity Methods
 
+        #region Inspector Variables
+        [FoldoutGroup("References")]
+        [SerializeField] private Animator _DoorAnimator;
+        #endregion Inspector Variables
+
         #region Private Variables
         private const string _PlayerTag = "player";
+        private DoorClosingInterrupt _InterruptStateBehavior;
         #endregion Private Variables
     }
 }
